3a398e49731344fef11d19c9f3e1f1137603eeea
ignatiush <ignatius.ian@gmail.com>
2017-12-07 17:44:09 -0600

    created and updating new branch

---
+++ source/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+
+class Dashboard extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Welcome to MP2!</h1>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
--- source/index.jsx
+++ source/index.jsx
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 
 // Include your new Components here
 import Home from './components/Home/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 // Include any new stylesheets here
 // Note that components' stylesheets should NOT be included here.
@@ -12,7 +13,7 @@ import Home from './components/Home/Home.jsx';
 require('./styles/main.scss');
 
 render(
-    <Home />,
+    <Dashboard />,
     // Define your router and replace <Home /> with it!
     document.getElementById('app')
 );
