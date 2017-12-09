71707e5154f98454125ee691c158f510caf45195
ignatiush <ignatius.ian@gmail.com>
2017-12-09 16:55:57 -0600

    added tab menu view for classes and history

--- source/components/Class/Class.jsx
+++ source/components/Class/Class.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Header, Menu, Container, Button, Card, Image, Icon, TextArea, Table } from 'semantic-ui-react'
+import { Header, Menu, Container, Segment} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import _ from 'lodash'
+
+
+import StudentClass from './StudentClass.jsx'
+import InstructorClass from './InstructorClass.jsx'
+
 import styles from './Class.scss'
 
 class Class extends Component {
@@ -10,60 +14,17 @@ class Class extends Component {
     // Constructor for component, calls to this component should pass in a classId param (i.e. /class/:id)
     constructor(){
         super();
-        let _dummyData = [
-            {upvotes: 5, time: 1, question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at ex nisl. Morbi malesuada erat non dui tristique pulvinar. Vestibulum at feugiat leo, eu tristique ante. In diam ex, accumsan vel turpis ut, feugiat lobortis ipsum. "},
-            {upvotes: 3, time: 3, question: "Etiam ex quam, lobortis eu efficitur nec, dictum quis turpis. Morbi non tempor lacus. Aliquam consectetur magna enim, eu dictum quam tempus ac."},
-            {upvotes: 1, time: 2, question: "Dummy Data 3"},
-        ];
         this.state = {
             classId: "",
             isInstructor: false,
-            studentQuestion: "",
-            sortColumn: "Upvotes",
-            direction: "descending",
-            questions: _.sortBy(_dummyData, ["Upvotes"])
+            activeItem: "session"
         }
     }
 
-    sortTable = clickedColumn => () => {
-
-        let questionData = this.state.questions;
-        let currentDirection = this.state.direction;
-        if (this.state.sortColumn !== clickedColumn) {
-            this.setState({
-                sortColumn: clickedColumn,
-                questions: _.sortBy(questionData, [clickedColumn]).reverse(),
-                direction: 'descending'
-            })
-        }else{
-            this.setState({
-                questions: questionData.reverse(),
-                direction: currentDirection === 'ascending' ? 'descending' : 'ascending',
-            })
-        }
-        console.log(this.state.sortColumn);
-        console.log(this.state.questions);
-    }
-
-    askQuestion = (event) => {
-        event.preventDefault();
-        let question = this.state.question;
-        this.setState({question:""});
-    }
-
-    generateQuestionRow = (questionObj, index) => {
-        return (
-            <Table.Row key={index}>
-                <Table.Cell textAlign='center'>{questionObj.upvotes}</Table.Cell>
-                <Table.Cell textAlign='center'>{questionObj.time}</Table.Cell>
-                <Table.Cell>{questionObj.question}</Table.Cell>
-            </Table.Row>
-        )
-    }
+    handleTabItemClick = (e, { name }) => this.setState({ activeItem: name })
 
     render() {
-        let sortBy = this.state.sortColumn;
-        let currentDirection = this.state.direction;
+        let activeItem = this.state.activeItem;
         return(
             <div>
                 <Menu fluid widths={3} borderless stackable>
@@ -83,29 +44,19 @@ class Class extends Component {
                         </Menu.Item>
                     </Container>
                 </Menu>
-                <Container className="questionSection">
-                    <h3>Ask A Question</h3>
-                    <TextArea value={this.state.studentQuestion} className="question" placeholder='Your Question Here...' autoHeight rows={3} onChange={(event,data) => this.setState({question: data.value})}/>
-                    <br></br>
-                    <Button className="submit" onClick={this.askQuestion}>
-                        Submit
-                    </Button>
-                </Container>
-                <Container className="questionList">
-                    <h3>Current Questions</h3>
-                    <Table sortable padded striped celled className="questionsTable">
-                        <Table.Header className="tableHeader">
-                            <Table.Row>
-                                <Table.HeaderCell sorted={sortBy === 'Upvotes' ? currentDirection : null} onClick={this.sortTable('upvotes')}>Upvotes</Table.HeaderCell>
-                                <Table.HeaderCell sorted={sortBy === 'Time' ? currentDirection : null} onClick={this.sortTable('time')}>Time</Table.HeaderCell>
-                                <Table.HeaderCell>Question</Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {this.state.questions.map(this.generateQuestionRow)}
-                       </Table.Body>
-                    </Table>
-                </Container>
+                <Menu attached='top' tabular className="tabMenu">
+                <Menu.Item name='session' active={activeItem === 'session'} onClick={this.handleTabItemClick}/>
+                <Menu.Item name='history' active={activeItem === 'history'} onClick={this.handleTabItemClick} />
+                </Menu>
+
+                <Segment attached='bottom'>
+                    {activeItem === 'session' ? (
+                        this.state.isInstructor ? <InstructorClass /> : <StudentClass />
+                        ) : (
+                        <p>History</p>
+                    )}
+                </Segment>
+
             </div>
         )
     }
@@ -115,9 +66,6 @@ class Class extends Component {
 Class.propTypes = {
     classId: PropTypes.string,
     isInstructor: PropTypes.bool,
-    studentQuestion: PropTypes.string,
-    questions: PropTypes.array
 }
-
 
 export default Class
